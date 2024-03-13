8a34387a7fabb6ee616c566a5df787773a922a30
shubhamcypher <shubhamdevo23@gmail.com>
2024-03-14 02:26:53 +0530

    adding app,jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./componets/Menu.jsx";
+import Navbar from "./componets/Navbar.jsx";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Video from "./pages/Video.jsx";
+import Signin from "./pages/SignIn.jsx";
+import Search from "./componets/Search.jsx";
 
+const Container = styled.div`
+  display : flex
+  
+`
+const Main = styled.div`
+  flex:7;
+  background-color: ${({theme})=>theme.bg};
+`
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = ()=>{
 
+  const [darkMode, setDarkMode] = useState(true)
+  
   return (
-    <>
-      <h1>hey</h1>
-    </>
+    <ThemeProvider theme={darkMode?darkTheme:lightTheme}>
+        <Container>
+          <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar/>
+            <Wrapper>
+            <Routes>
+                <Route path="/">
+                  <Route index element={<Home type= 'random' />} />
+                  <Route path="trending" element={<Home type= 'trending' />} />
+                  <Route path="subscriptions" element={<Home type= '/subscribed' />} />
+                  <Route path="search" element={<Search type= '/search' />} />
+        
+                  <Route path="signin" element={<Signin />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+          </BrowserRouter>
+        </Container>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;

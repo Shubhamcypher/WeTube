3434855d05c378d16b56abc7c738367ca2cf254c
shubhamcypher <shubhamdevo23@gmail.com>
2024-03-14 02:32:03 +0530

    adding pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Card from '../componets/card/Card.jsx'
+import axios from 'axios'
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    
+`
+
+const Home = ({type}) => {
+  
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const fetchVideos = async()=>{
+      const res = await axios.get(`/api/video/${type}`);
+      
+      setVideos(res.data) ;//this data contains everything that api sends us
+    };
+    fetchVideos();
+  }, [type])
+  
+  return (
+    <Container>
+        {videos.map((video)=>(
+          <Card key={video._id} video={video}/>
+        ))}
+    </Container>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,155 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import GoogleIcon from '@mui/icons-material/Google';
+import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
+import { auth,provider } from "../firebase.js";
+import {signInWithPopup} from 'firebase/auth'
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 6px);
+  color: ${({ theme }) => theme.text};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.bgLighter};
+  border: 1px solid ${({ theme }) => theme.soft};
+  padding: 20px 50px;
+  gap: 10px;
+  border-radius: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+`;
+
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+  width: 100%;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const More = styled.div`
+  display: flex;
+  margin-top: 10px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Links = styled.div`
+  margin-left: 30px;
+`;
+
+const Link = styled.span`
+  margin-left: 20px;
+`;
+
+const GoogleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-items:center;
+`
+
+const SignIn = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+
+  const navigate = useNavigate()
+
+  const handleLogin = async (e)=>{
+    e.preventDefault();
+    dispatch(loginStart())
+    try {
+      const res = await axios.post("api/auth/signin",{name,password});
+      dispatch(loginSuccess(res.data))
+      navigate('/')
+    } 
+    catch (error) {
+      dispatch(loginFailure())
+      
+    }
+  }
+
+  const signinWithGoogle = async()=>{
+    dispatch(loginStart())
+    signInWithPopup(auth,provider)
+    .then((result)=>{
+      axios.post("api/auth/googleAuth",{
+        name: result.user.displayName,
+        email: result.user.email,
+        img: result.user.photoURL
+      })
+      .then((res)=>{
+        dispatch(loginSuccess(res.data))
+        navigate('/')
+      })
+    })
+    .catch((error)=>{
+      dispatch(loginFailure())
+      console.log(error);
+    })
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Sign in</Title>
+        <SubTitle>to continue to WeTube</SubTitle>
+        <Input placeholder="username" onChange={e=>setName(e.target.value)} />
+        <Input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)} />
+        <Button  onClick={handleLogin}>Sign in</Button>
+        <Title>or</Title>
+
+        <GoogleContainer>
+        <Button onClick={signinWithGoogle}><GoogleIcon/>Log in with google</Button>
+        </GoogleContainer>
+        <Title>or</Title>
+        <Title>Sign up</Title>
+        <Input placeholder="username" />
+        <Input placeholder="email" />
+        <Input type="password" placeholder="password" />
+        <Button>Sign up</Button>
+      </Wrapper>
+      <More>
+        English(USA)
+        <Links>
+          <Link>Help</Link>
+          <Link>Privacy</Link>
+          <Link>Terms</Link>
+        </Links>
+      </More>
+    </Container>
+  );
+};
+
+export default SignIn;

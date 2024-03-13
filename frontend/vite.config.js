1dfd6cfddd9fbee7b5001628da1998e0fdeb6391
shubhamcypher <shubhamdevo23@gmail.com>
2024-03-14 02:35:16 +0530

    adding proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        
+        cors: true, // Enable CORS for preflight requests (OPTIONS)
+      },
+    },
+  },
   plugins: [react()],
 })

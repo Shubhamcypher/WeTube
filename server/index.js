36ecccb4803d30e4a79dcfff2925086658f9975d
shubhamcypher <shubhamdevo23@gmail.com>
2024-03-15 00:54:20 +0530

    port add

--- server/index.js
+++ server/index.js
@@ -54,5 +54,5 @@ app.use((err, req, res, next) => {
 
 app.listen(process.env.PORT,()=>{
     connect();
-    console.log(`Server is running on port 8000 `);
+    console.log(`Server is running on port ${process.env.PORT} `);
 })

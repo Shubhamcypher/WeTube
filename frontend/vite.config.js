b7ab39b9899c0c5e64c1fc0a1e1c8889f5bd3a90
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-08 03:23:21 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/',
+        target: 'https://we-tube-a4dz.vercel.app/',
         changeOrigin: true,
         secure: true,
       },

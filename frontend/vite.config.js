3a4475b43367348b63658a5587050d9492c44374
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-08 02:52:46 +0530

    yep

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://we-tube-server-psi.vercel.app/'?'https://we-tube-server-psi.vercel.app/':'http://localhost:8000',
-        secure: true,
+        changeOrigin: true,
+        secure: false,
       },
     },
   },

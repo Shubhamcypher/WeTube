9f29b145366fd929057ee8590539d52fa14692cc
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-08 05:52:59 +0530

    changeOrigin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://wetube-cmtu.onrender.com/',
-        changeOrigin: true,
         secure: true,
       },
     },

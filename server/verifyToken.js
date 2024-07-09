93c242ceaf5023233c263a6a96a979c3493e5b22
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-09 19:02:15 +0530

    verifyToken

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -8,7 +8,7 @@ export const verifyToken = (req,res,next)=>{
     if(!token) return next(createError(400,"You are not authenticated mannnnnnnnnnn!"))
     
 
-    //here we are checking if the token matches, if not matches it will have error and the if condition follows if no erros req.user ko database wale user ka access mil jyga
+    //here we are checking if the token matches, if not matches it will have error and if condition follows if no erros req.user ko database wale user ka access mil jyga
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err,user)=>{
         if(err) return next(createError(400,"Token verification failed!!"));
 

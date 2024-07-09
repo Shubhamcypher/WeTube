59da810bf0731ea48d9d35da84035ed49b4dca24
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-09 22:51:59 +0530

    token json

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -52,7 +52,7 @@ export const signin = async (req,res,next)=>{
         res
         .cookie("access_token",token,options)
         .status(200)
-        .json("Login successful")
+        .json(token)
 
         
 

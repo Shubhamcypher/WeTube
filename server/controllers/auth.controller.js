389def88ddfd9b9f77e86adfd03e693b871a4fc6
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-12 18:19:32 +0530

    setting refresh token timing

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -47,7 +47,7 @@ export const signin = async (req, res, next) => {
         const newRefreshToken =  new refreshTokenModel({
             userId: user._id,
             token: refreshToken,
-            expiresAt: new Date(Date.now() + 15 * 60 * 1000) // 15 minutes from now
+            expiresAt: new Date(Date.now() + 2 * 60* 60 * 1000) // 15 minutes from now
         });
         await newRefreshToken.save();
 

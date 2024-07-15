2b9937d6b9f70f51861c7a4d8c5a233a30a7b892
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-15 22:21:38 +0530

    making the difference

--- frontend/src/componets/Navbar.jsx
+++ frontend/src/componets/Navbar.jsx
@@ -8,10 +8,7 @@ import VideoCallIcon from '@mui/icons-material/VideoCall';
 import Alert from '@mui/material/Alert';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
-
-
-
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Upload from './Upload';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../redux/userSlice';
@@ -20,14 +17,14 @@ import MyProfile from './MyProfile';
 const Container = styled.div`
   position: sticky;
   top: 0;
-  background-color:${({theme})=>theme.bgLighter};
+  background-color:${({ theme }) => theme.bgLighter};
   height: 56px;
-  z-index:999;
-  display:flex;
+  z-index: 999;
+  display: flex;
   flex-direction: row-reverse;
-  gap:10px;
-  `
-  const Wrapper = styled.div`
+  gap: 10px;
+`
+const Wrapper = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
@@ -40,91 +37,90 @@ const Search = styled.div`
   left: 0px;
   right: 0px;
   margin: auto;
-  width:40%;
+  width: 40%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 5px;
   border: 1px solid #ccc;
   border-radius: 7px;
-  color: ${({theme})=>theme.text};
+  color: ${({ theme }) => theme.text};
 
   @media (max-width: 720px) { 
-    position:relative;
-    width:50%
+    position: relative;
+    width: 50%
 }
 `
 
 const Input = styled.input`
-  border:none;
+  border: none;
   background-color: transparent;
-  width:90%;
-  outline:none;
-  color: ${({theme})=>theme.text};
+  width: 90%;
+  outline: none;
+  color: ${({ theme }) => theme.text};
 `
 
 const Button = styled.button`
-    padding: 5px 15px;
-    background-color: transparent ;
-    color: #3ae;
-    border: 1px solid #3ae;
-    border-radius:3px;
-    font-weight: 500;
-    cursor: pointer;
+  padding: 5px 15px;
+  background-color: transparent;
+  color: #3ae;
+  border: 1px solid #3ae;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
 `;
 
 const User = styled.div`
-      display:flex;
-      align-items:center;
-      gap:20px;
-      color:  ${({theme})=>theme.text};
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  color:  ${({ theme }) => theme.text};
 `
 
 const Avatar = styled.img`
-      width: 32px;
-      height: 32px;
-      border-radius: 50%;
-      background-color: #999;
-      object-fit:cover;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: #999;
+  object-fit: cover;
 `;
-
 
 const VideoButton = styled(VideoCallIcon)`
   cursor: pointer;
-  
+
   &:hover {
     color: #00B5E2;
     transform: scale(1.3);
-     transition: transform 0.8s ease, color 0.8s ease;
+    transition: transform 0.8s ease, color 0.8s ease;
   }
 `;
 
 const StyledAlert = styled(Alert)`
-  background-color: ${({theme})=>theme.bgLighter} !important;
-  color: ${({theme})=>theme.text} !important;
+  background-color: ${({ theme }) => theme.bgLighter} !important;
+  color: ${({ theme }) => theme.text} !important;
   border-radius: 8px;
   margin: 10px;
-  position:fixed;
-  z-index:999;
-  top:10;
-  display:flex;
-  width:50vw;
+  position: fixed;
+  z-index: 999;
+  top: 10;
+  display: flex;
+  width: 50vw;
 
-   @media (max-width: 720px) { 
-    width:80vw;
-    margin:2px 25px;
+  @media (max-width: 720px) { 
+    width: 80vw;
+    margin: 2px 25px;
 }
 `;
 
 const LogoutText = styled.div`
-    @media (max-width: 720px) { 
-    display:none;
+  @media (max-width: 720px) { 
+    display: none;
 }
 `;
 
 const DeleteAccountText = styled.div`
-    @media (max-width: 720px) { 
-    display:none;
+  @media (max-width: 720px) { 
+    display: none;
 }
 `;
 
@@ -137,7 +133,7 @@ const AlertButton = styled.button`
   cursor: pointer;
   font-weight: bold;
   margin-right: 8px;
-  position:relative;
+  position: relative;
 
   &:hover {
     opacity: 0.8;
@@ -145,174 +141,163 @@ const AlertButton = styled.button`
 `;
 
 const ButtonWrapper = styled.div`
-    position: absolute;
-    top: 50%;
-    right: 10px;
-    transform: translateY(-50%);
-    display: flex;
-    gap: 0px;
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  display: flex;
+  gap: 0px;
 `;
 
+const Navbar = ({ darkMode, setDarkMode }) => {
+  const { currentUser } = useSelector(state => state.user)
+  const [open, setOpen] = useState(false)
+  const [openProfile, setOpenProfile] = useState(false)
+  const [q, setQ] = useState("")
+  const [showAlert, setShowAlert] = useState(false);
+  const [deleteAccountMenu, setDeleteAccountMenu] = useState(false)
 
+  const alertRef = useRef(null);
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (alertRef.current && !alertRef.current.contains(e.target)) {
+        setShowAlert(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [setShowAlert]);
 
-
-
-
-const Navbar = ({darkMode, setDarkMode}) => {
-const {currentUser} = useSelector(state=>state.user)
-const [open, setOpen] = useState(false)
-const [openProfile, setOpenProfile] = useState(false)
-const [q, setQ] = useState("")
-const [showAlert, setShowAlert] = useState(false);
-const [deleteAccountMenu, setDeleteAccountMenu] = useState(false)
-const [avatarUrl, setAvatarUrl] = useState(currentUser?.img);
-
-const alertlRef = useRef(null);
-
-
-
-const navigate = useNavigate()
-const dispatch = useDispatch()
-
-
-
-useEffect(() => {
-  const handleClickOutside = (e) => {
-    if (alertlRef.current && !alertlRef.current.contains(e.target)) {
-      setShowAlert(false);
-    }
+  const handleSwitch = async () => {
+    alert(`Logging out, ${currentUser.name}`)
+    dispatch(logout())
+    console.log("User logged out from database");
+    setShowAlert(false);
+    navigate('/signin');
+    await axios.get(`/auth/logout`);
   };
-  document.addEventListener('mousedown', handleClickOutside);
-  return () => {
-    document.removeEventListener('mousedown', handleClickOutside);
+
+  const handleCancel = () => {
+    setShowAlert(false);
   };
-}, [setShowAlert]);
 
-const handleSwitch = async() => {
-  alert(`Logging out, ${currentUser.name}` )
-  dispatch(logout())
-  console.log("User logged out from database");
-  setShowAlert(false);
-  navigate('/signin');
-  await axios.get(`/auth/logout`);
-};
+  const handleLogoutAndContinue = async () => {
+    alert(`Logging out, ${currentUser.name}`)
+    dispatch(logout())
+    console.log("User logged out from database");
+    setShowAlert(false);
+    navigate('/')
+    await axios.get(`/auth/logout`);
+  };
 
-const handleCancel = () => {
-  setShowAlert(false);
-};
+  const handleYesDeleteAccount = async () => {
+    setDeleteAccountMenu(false)
+    await axios.delete(`/user/${currentUser._id}`)
+    dispatch(logout())
+    navigate('/signin')
 
-const handleLogoutAndContinue = async() => {
-  alert(`Logging out, ${currentUser.name}` )
-  dispatch(logout())
-  console.log("User logged out from database");
-  setShowAlert(false);
-  navigate('/')
-  await axios.get(`/auth/logout`);
-};
-
-const handleYesDeleteAccount = async()=>{
-  setDeleteAccountMenu(false)
-  await axios.delete(`/user/${currentUser._id}`)
-  dispatch(logout())
-  navigate('/signin')
-  
-}
-const handleNoDeleteAccount =()=>{
-  setDeleteAccountMenu(false)
-}
-
-const handleKeyPress = (event) => {
-  if (event.key === 'Enter') {
-    q && navigate(`/search?q=${q}`);
   }
-};
+  const handleNoDeleteAccount = () => {
+    setDeleteAccountMenu(false)
+  }
 
-useEffect(() => {
-  const fetchUserData = async () => {
-    try {
-      const response = await axios.get(`/user/${currentUser._id}`);
-      const userData = response.data;
-      setAvatarUrl(userData.img);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      q && navigate(`/search?q=${q}`);
     }
   };
 
-  fetchUserData();
-}, [currentUser]);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`/user/${currentUser._id}`);
+        const userData = response.data;
+        if (userData.img !== currentUser.img) {
+          dispatch(updateUserData(userData));
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, [currentUser, dispatch]);
 
   return (
     <>    
-    <Container>
-      <Wrapper>
-        <Search>
-          <Input 
-            placeholder="Search" 
-            onChange={(e)=>setQ(e.target.value)}
-            onKeyDown={handleKeyPress}/>
-          <SearchIcon onClick={ ()=> q && navigate(`/search?q=${q}`)}   style={{cursor:'pointer'}} />
-        </Search>
-        {currentUser  ?(
-          <User>
-            <VideoButton onClick={()=>setOpen(true)}  fontSize="large"/>
-            <NotificationsIcon fontSize="large"/>
-            <Avatar src={currentUser?.img} style={{cursor:'pointer'} } onClick={()=>{
-              setOpenProfile(!openProfile)
-              }}/>
-          </User>
-        ) : <Link to='/signin' style={{textDecoration:"none"}}>
+      <Container>
+        <Wrapper>
+          <Search>
+            <Input
+              placeholder="Search"
+              onChange={(e) => setQ(e.target.value)}
+              onKeyDown={handleKeyPress} />
+            <SearchIcon onClick={() => q && navigate(`/search?q=${q}`)} style={{ cursor: 'pointer' }} />
+          </Search>
+          {currentUser ? (
+            <User>
+              <VideoButton onClick={() => setOpen(true)} fontSize="large" />
+              <NotificationsIcon fontSize="large" />
+              <Avatar src={currentUser?.img} style={{ cursor: 'pointer' }} onClick={() => {
+                setOpenProfile(!openProfile)
+              }} />
+            </User>
+          ) : <Link to='/signin' style={{ textDecoration: "none" }}>
             <Button>
-                <PersonIcon fontSize="sm"/> Sign in
+              <PersonIcon fontSize="sm" /> Sign in
             </Button>
-        </Link>}     
-      </Wrapper>
-    </Container>
+          </Link>}
+        </Wrapper>
+      </Container>
 
+      {open && <Upload setOpen={setOpen} />}
 
-    {open && <Upload setOpen={setOpen} />}
-
-    {showAlert && (
-        <StyledAlert severity="warning" ref={alertlRef}>
+      {showAlert && (
+        <StyledAlert severity="warning" ref={alertRef}>
           <div>
-          <LogoutText >
-          You won't be able to use complete features.
-          </LogoutText>
-          <div>
-          <ButtonWrapper>
-          <AlertButton onClick={handleSwitch} color="blue">
-            Switch Account
-          </AlertButton>
-          <AlertButton onClick={handleCancel} color="green" >
-            Cancel
-          </AlertButton>
-          <AlertButton onClick={handleLogoutAndContinue} color="red">
-            Logout
-          </AlertButton>
-          </ButtonWrapper>
-          </div>
+            <LogoutText >
+              You won't be able to use complete features.
+            </LogoutText>
+            <div>
+              <ButtonWrapper>
+                <AlertButton onClick={handleSwitch} color="blue">
+                  Switch Account
+                </AlertButton>
+                <AlertButton onClick={handleCancel} color="green" >
+                  Cancel
+                </AlertButton>
+                <AlertButton onClick={handleLogoutAndContinue} color="red">
+                  Logout
+                </AlertButton>
+              </ButtonWrapper>
+            </div>
           </div>
         </StyledAlert>
       )}
 
       {deleteAccountMenu && (
         <StyledAlert severity="warning" >
-        <DeleteAccountText>
-        Are you sure want to delete {currentUser.name}? 
-        </DeleteAccountText>
-        <ButtonWrapper>
-        <AlertButton onClick={handleYesDeleteAccount} color="red">
-          Yes
-        </AlertButton>
-        <AlertButton onClick={handleNoDeleteAccount} color="green" >
-          No
-        </AlertButton>
-        </ButtonWrapper>
-      </StyledAlert>
+          <DeleteAccountText>
+            Are you sure want to delete {currentUser.name}?
+          </DeleteAccountText>
+          <ButtonWrapper>
+            <AlertButton onClick={handleYesDeleteAccount} color="red">
+              Yes
+            </AlertButton>
+            <AlertButton onClick={handleNoDeleteAccount} color="green" >
+              No
+            </AlertButton>
+          </ButtonWrapper>
+        </StyledAlert>
       )}
 
-      {openProfile && <MyProfile setOpenProfile={setOpenProfile} setShowAlert={setShowAlert} darkMode={darkMode} setDarkMode={setDarkMode} setOpen={setOpen} setDeleteAccountMenu={setDeleteAccountMenu}/>}
+      {openProfile && <MyProfile setOpenProfile={setOpenProfile} setShowAlert={setShowAlert} darkMode={darkMode} setDarkMode={setDarkMode} setOpen={setOpen} setDeleteAccountMenu={setDeleteAccountMenu} />}
     </>
   )
 }

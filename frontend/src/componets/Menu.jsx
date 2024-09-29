4cd6ae1ae03a9d0ea6f4a186f59cc99efda01b2e
shubhamcypher <shubhamdevo23@gmail.com>
2024-09-29 23:40:07 +0530

    done here

--- frontend/src/componets/Menu.jsx
+++ frontend/src/componets/Menu.jsx
@@ -243,7 +243,7 @@ const Menu = ({ darkMode, setDarkMode, setShowMenu, showMenu }) => {
         <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
           <Logo>
             <Img src={channel_logo} />
-            <LogoText>WexTube</LogoText>
+            <LogoText>WeTube</LogoText>
           </Logo>
         </Link>
         </TopMenu>
